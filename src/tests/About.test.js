7135b14830a5b01bc0f3f6d3fa2932321c820563
6u7o <augustopinheiro19999999@gmail.com>
2022-02-18 17:10:40 -0300

    req2: Testing About page elements

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Testando o componente App', () => {
+  test(
+    'Verifica os elementos de About',
+    () => {
+      render(<About />);
+
+      const h1El = screen.getByRole('heading', { name: /About Pokédex/i });
+
+      expect(h1El).toBeInTheDocument();
+
+      const articlesEl = screen.getAllByText(/Pokédex/i);
+
+      expect(articlesEl.length).toBe(2);
+
+      const imgEl = screen.getByRole('img', {
+        name: /pokédex/i,
+      });
+
+      expect(imgEl.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    },
+  );
+});
