ba9c90c03741990223441810681ee593f1f3368c
6u7o <augustopinheiro19999999@gmail.com>
2022-02-18 19:13:03 -0300

    req3: Testing if favorited pokemon appears in the list

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,43 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testando o componente App', () => {
+  test(
+    'Verifica o parágrafo em FavoritePokemons',
+    () => {
+      render(<FavoritePokemons />);
+
+      const pEl = screen.getByText(/no favorite pokemon found/i);
+      expect(pEl).toBeInTheDocument();
+      // screen.logTestingPlaygroundURL();
+    },
+  );
+  test(
+    'Verifica se os pokemons favoritados aparecem na página',
+    () => {
+      render(<App />, { wrapper: MemoryRouter });
+      const moreDetailsEl = screen.getByRole('link', {
+        name: /more details/i,
+      });
+      userEvent.click(moreDetailsEl);
+
+      const checkboxEl = screen.getByRole('checkbox');
+
+      userEvent.click(checkboxEl);
+
+      const favsEl = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+      userEvent.click(favsEl);
+
+      const pikachuEl = screen.getByText(/Pikachu/i);
+
+      expect(pikachuEl).toBeInTheDocument();
+
+      // screen.logTestingPlaygroundURL();
+    },
+  );
+});
